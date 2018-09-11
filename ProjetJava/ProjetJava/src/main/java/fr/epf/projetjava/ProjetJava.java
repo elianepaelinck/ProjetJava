7dc169259f9c565b0f522386a0ce0828cfcf5ae2
Valentin <valentin.mazhar@gmail.com>
2018-09-11 17:17:55 +0200

    les galères continuent

--- ProjetJava/ProjetJava/src/main/java/fr/epf/projetjava/ProjetJava.java
+++ ProjetJava/ProjetJava/src/main/java/fr/epf/projetjava/ProjetJava.java
@@ -1,6 +1,10 @@
 package fr.epf.projetjava;
 
+import fr.epf.projetjava.models.Project;
+import fr.epf.projetjava.models.Task;
 import fr.epf.projetjava.models.User;
+import fr.epf.projetjava.persistence.ProjectDao;
+import fr.epf.projetjava.persistence.TaskDao;
 import fr.epf.projetjava.persistence.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -15,6 +20,8 @@ public class ProjetJava {
   
   @Autowired
   private UserDao userDao;
+  private ProjectDao projectDao;
+  private TaskDao taskDao;
   
   public static void main(String[] args) {
     // Point d'entrée de l'application.
@@ -22,7 +29,9 @@ public class ProjetJava {
     // Il va aller "regarder" nos classes et détecter les annotations des singletons 
     // (@Controller, @Repository, @Component, @Service, etc...)
     // Ensuite, il va construire l'arbre de dépendances et le résoudre en injectant les données dans les bonnes classes
+
     SpringApplication.run(ProjetJava.class);
+
   }
 
   /**
@@ -35,5 +44,24 @@ public class ProjetJava {
     userDao.save(new User(null,"Loic", "Ortola"));
     userDao.save(new User(null, "Ambroise", "Soullier"));
     userDao.save(new User(null, "Harry", "Covert"));
+
+    User luc = new User(null,"luc","lebert");
+    User val = new User(null,"val","mazhar");
+
+    ArrayList testU = new ArrayList();
+    testU.add(luc);
+    testU.add(val);
+
+    ArrayList testT = new ArrayList();
+    testT.add(new Task(null,"tache de luc",null,"ceci est ma tache", luc));
+
+//    Project testProject = new Project (null,"new projet", testU, testT);
+
+//    projectDao.deleteAll();
+//    projectDao.save(testProject);
+
+      taskDao.deleteAll();
+      taskDao.save(new Task(null,"tache de luc",false,"ceci est ma tache", luc));
+
   }
 }
