08e1c2f7c4f56b53aee2c03943fe51645cc08cb4
llebert <43138101+llebert@users.noreply.github.com>
2018-09-13 11:23:10 +0200

    mis au propre

--- ProjetJava/ProjetJava/src/main/java/fr/epf/projetjava/ProjetJava.java
+++ ProjetJava/ProjetJava/src/main/java/fr/epf/projetjava/ProjetJava.java
@@ -1,6 +1,10 @@
 package fr.epf.projetjava;
 
+import fr.epf.projetjava.models.Project;
+import fr.epf.projetjava.models.Task;
 import fr.epf.projetjava.models.User;
+import fr.epf.projetjava.persistence.ProjectDao;
+import fr.epf.projetjava.persistence.TaskDao;
 import fr.epf.projetjava.persistence.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -8,21 +12,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class ProjetJava {
-  
+
   @Autowired
   private UserDao userDao;
-  
+  private ProjectDao projectDao;
+  private TaskDao taskDao;
+
   public static void main(String[] args) {
     // Point d'entrée de l'application.
     // On dit à Spring de s'initialiser
-    // Il va aller "regarder" nos classes et détecter les annotations des singletons 
+    // Il va aller "regarder" nos classes et détecter les annotations des singletons
     // (@Controller, @Repository, @Component, @Service, etc...)
     // Ensuite, il va construire l'arbre de dépendances et le résoudre en injectant les données dans les bonnes classes
+
     SpringApplication.run(ProjetJava.class);
+
   }
 
   /**
@@ -32,8 +41,27 @@ public class ProjetJava {
   @PostConstruct
   public void init() {
     userDao.deleteAll();
-    userDao.save(new User("loic@mail.com","Loic", "Ortola", "password"));
-    userDao.save(new User("ambroise@mail.com", "Ambroise", "Soullier","password"));
-    userDao.save(new User("harry@mail.com", "Harry", "Covert", "password"));
+    userDao.save(new User(null,"Loic", "Ortola"));
+    userDao.save(new User(null, "Ambroise", "Soullier"));
+    userDao.save(new User(null, "Harry", "Covert"));
+
+    User luc = new User(null,"luc","lebert");
+    User val = new User(null,"val","mazhar");
+    userDao.save(luc);
+    ArrayList testU = new ArrayList();
+//    testU.add(luc);
+//    testU.add(val);
+//
+    ArrayList testT = new ArrayList();
+    testT.add(new Task(null,"tache de luc",null,"ceci est ma tache", luc));
+
+//    Project testProject = new Project (null,"new projet", testU, testT);
+
+//    projectDao.deleteAll();
+//    projectDao.save(testProject);
+
+    //taskDao.deleteAll();
+    //taskDao.save(new Task(null,"tache de luc",false,"ceci est ma tache", luc));
+
   }
 }
