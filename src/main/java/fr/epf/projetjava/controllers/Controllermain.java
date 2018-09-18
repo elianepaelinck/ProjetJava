4c42aab7e870e7dae1640cdb01604ae25912dea0
elianepaelinck <eliane.paelinck@epfedu.fr>
2018-09-18 17:03:14 +0200

    ajout projet OK

--- src/main/java/fr/epf/projetjava/controllers/Controllermain.java
+++ src/main/java/fr/epf/projetjava/controllers/Controllermain.java
@@ -3,12 +3,12 @@ package fr.epf.projetjava.controllers;
 import fr.epf.projetjava.models.Project;
 import fr.epf.projetjava.persistence.ProjectDao;
 import fr.epf.projetjava.persistence.UserDao;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Controllermain {
@@ -25,26 +25,26 @@ public class Controllermain {
     public String index(Model model) {
         model.addAttribute("datauser", userDao.findAll());
         model.addAttribute("dataproj", projectDao.findAll());
+        model.addAttribute("projet", new Project());
         return "index";
     }
-
+/*
     @GetMapping("/projet")
     public String projet(Model model) {
         model.addAttribute("datauser", userDao.findAll());
         return "projet";
-    }
-
-  /*  @RequestMapping(value = "offers/{id}", method = RequestMethod.POST)
-    public String delete(@PathVariable("id") int itemId, Model model) {
-        Project project = projectDao.findById(itemId).get();
-        projectDao.delete(project);
-        return "redirect:/offers";
     }*/
 
-    @RequestMapping(value = "index/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "index/{id}")
     public String show(@PathVariable("id") int itemId, Model model) {
         Project project = projectDao.findById(itemId).get();
         model.addAttribute("projet",project);
         return "projet";
+    }
+
+    @PostMapping("/index")
+    public String addProject(Project project, Model model) {
+        projectDao.save(project);
+        return "redirect:/index";
     }
 }
--- src/main/java/fr/epf/projetjava/controllers/UsersController.java
+++ src/main/java/fr/epf/projetjava/controllers/UsersController.java
@@ -1,7 +1,6 @@
 package fr.epf.projetjava.controllers;
 
 import fr.epf.projetjava.models.User;
-import fr.epf.projetjava.persistence.ProjectDao;
 import fr.epf.projetjava.persistence.UserDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,24 +16,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UsersController {
 
     private final UserDao userDao;
-    private ProjectsController project;
 
     public UsersController(UserDao userDao) {
         this.userDao = userDao;
     }
 
-
-    /**
-     * Ceci sera mappé sur l'URL '/users'.
-     * C'est le routeur de Spring MVC qui va détecter et appeler directement cette méthode.
-     * Il lui fournira un "modèle", auquel on pourra rajouter des attributs.
-     * Ce modèle sera ensuite forwardé à une page web (dans resources/templates).
-     * Le nom de la template est retourné par la fonction. Ici, elle appelle donc le template "users".
-     *
-     * @param model le modèle
-     * @return
-     */
-    @GetMapping("/users")
+    /*@GetMapping("/users")
     public String getUsers(Model model) {
         model.addAttribute("data", userDao.findAll());
         return "users-list";
@@ -52,20 +39,9 @@ public class UsersController {
         return "redirect:/users";
     }
 
-    /*@GetMapping("/index")
-    public String index(Model model) {
-        model.addAttribute("datauser", userDao.findAll());
-        model.addAttribute("dataproj", project.getProject(model));
-        return "index";
-    }*/
-
     @GetMapping("/login")
     public String login(Model model) {
         return "login";
-    }
-/*
-    @GetMapping("/projet")
-    public String projet(Model model) {
-        return "projet";
     }*/
+
 }
